55e927d56b5076e76cc4bace809ba9eff189dfde
priyam-anand <priyam27.anand@gmail.com>
2023-08-29 11:30:25 +0530

    feat: dockerized heimdall service

--- heimdall/config/configuration.ts
+++ heimdall/config/configuration.ts
@@ -1,15 +1,18 @@
-export default () => ({
-  PORT: parseInt(process.env.PORT) || 3000,
-  NONCE: {
-    SECRET: process.env.NONCE_SECRET,
-    EXPIRES_IN: process.env.NONCE_EXPIRES_IN,
-  },
-  ACCESS_TOKEN: {
-    SECRET: process.env.ACCESS_TOKEN_SECRET,
-    EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
-  },
-  REFRESH_TOKEN: {
-    SECRET: process.env.REFRESH_TOKEN_SECRET,
-    EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
-  },
-});
+export default () => {
+  // console.log(process.env);
+  return {
+    PORT: parseInt(process.env.PORT) || 3000,
+    NONCE: {
+      SECRET: process.env.NONCE_SECRET,
+      EXPIRES_IN: process.env.NONCE_EXPIRES_IN,
+    },
+    ACCESS_TOKEN: {
+      SECRET: process.env.ACCESS_TOKEN_SECRET,
+      EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    },
+    REFRESH_TOKEN: {
+      SECRET: process.env.REFRESH_TOKEN_SECRET,
+      EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    },
+  };
+};
